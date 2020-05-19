0f89b8e5c52f55da8d930078911626eb566e1503
jonaskay <jonaskay@iki.fi>
2020-05-19 12:45:16 +0300

    Export Heading and Markdown components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import FooterComponent from "./components/footer";
+import HeadingComponent from "./components/heading";
+import MarkdownComponent from "./components/markdown";
 
 export const Footer = FooterComponent;
+export const Heading = HeadingComponent;
+export const Markdown = MarkdownComponent;
