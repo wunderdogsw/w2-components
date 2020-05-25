adc40881811c9e7559407900b9644695e5a9ff0c
jonaskay <jonaskay@iki.fi>
2020-05-25 14:32:09 +0300

    Add Container component

---
+++ src/components/container.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Container from "./container";
+
+export default { title: "Container" };
+
+export const plain = () => (
+  <Container>
+    <h1>Lorem ipsum</h1>
+    <h2>Lorem ipsum</h2>
+    <p>Lorem ipsum</p>
+  </Container>
+);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import CardComponent from "./components/card";
+import ContainerComponent from "./components/container";
 import FooterComponent from "./components/footer";
 import HeadingComponent from "./components/heading";
 import ListComponent from "./components/list";
 import MarkdownComponent from "./components/markdown";
 
 export const Card = CardComponent;
+export const Container = ContainerComponent;
 export const Footer = FooterComponent;
 export const Heading = HeadingComponent;
 export const List = ListComponent;
